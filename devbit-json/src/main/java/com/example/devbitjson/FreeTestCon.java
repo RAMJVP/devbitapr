e45928ef47ce102d2daa1109f7bf16b03e2602c2
like_ <like_@host.docker.internal>
2024-05-13 20:03:00 +0530

    gift

--- devbit-json/src/main/java/com/example/devbitjson/FreeTestCon.java
+++ devbit-json/src/main/java/com/example/devbitjson/FreeTestCon.java
@@ -3,12 +3,9 @@ package com.example.devbitjson;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,45 +28,39 @@ public class FreeTestCon {
 				
 				
 				
+				
+				
+				"https://drive.google.com/file/d/1bMIdz1M2SH2WSYE7MvhHpd0jKlbS3uVv/view?usp=drivesdk",
+				"https://drive.google.com/file/d/1usN68tP0L35ri8SonBoj8CbjepyAEFMJ/view?usp=drivesdk",
+				"https://drive.google.com/file/d/1DpNObnVP8EKxPMqYQaulAiv5KHTJA6Oj/view?usp=drivesdk",
+				"https://drive.google.com/file/d/1-pe7A6TJ09ljpZ1N55LA6EsygfoYNPrd/view?usp=drivesdk",
+				"https://drive.google.com/file/d/12UpeY_aa4-L-Fh5pbqxcUF5rnaMHK1-N/view?usp=drivesdk",
+				"https://drive.google.com/file/d/1KFVgsYvLGoIqv25E179q__BNnYn3hvLl/view?usp=drivesdk",
+				"https://drive.google.com/file/d/1VmDkWfa6rXKSe_g1K8c-SWrv31mT2ikP/view?usp=drivesdk",
+				"https://drive.google.com/file/d/1fbdH7JhzU3kobepUHhXraeFZkh64PkYR/view?usp=drivesdk",
+				"https://drive.google.com/file/d/1aLtGp38nRK1fO9Dc-MwtR_cxQ_Zsepyr/view?usp=drivesdk",
+				"https://drive.google.com/file/d/1mFiRqKGorfkraeF4IoBK3r0F188xoRAh/view?usp=drivesdk",
+				"https://drive.google.com/file/d/18C3c9shLV4X5wdoHi9opGkOL8H_veh06/view?usp=drivesdk",
+				"https://drive.google.com/file/d/1N6wG4DwiqlOGSQ0NbToQeaUfsb8j3dxH/view?usp=drivesdk",
+				"https://drive.google.com/file/d/1PoGcr6cqqaPfxH4Z_Jk_TwcWhxKgHKiM/view?usp=drivesdk",
+				"https://drive.google.com/file/d/1QKsJbrJ4Es8sQDtnmK9gdwVaaSMrIfN3/view?usp=drivesdk",
+				"https://drive.google.com/file/d/1SelLTqk7Wnrcg7T_9v3bkBFspC74td98/view?usp=drivesdk",
+				"https://drive.google.com/file/d/1rxtxDx_1Z8HdmqEbWSR3n3IXM6JfhCMZ/view?usp=drivesdk",
+				"https://drive.google.com/file/d/1zfT1TXXErg0gOEErk0KPCWcH6RRjhrC1/view?usp=drivesdk",
+				"https://drive.google.com/file/d/1yJT1QBlYIDqIZ-dVE-axq9H6Ix1eb-Ux/view?usp=drivesdk",
+				"https://drive.google.com/file/d/1DDxPats4lK4poaiK495RILjzOxr4u7Ew/view?usp=drivesdk",
+				"https://drive.google.com/file/d/1JtTOSu2w7YY5gh1VYcCmw9O8peT0EtNW/view?usp=drivesdk",
+				"https://drive.google.com/file/d/1EHlFaAFwNF8UJW4aTwDnyeYQ-bePl_TF/view?usp=drivesdk",
+				"https://drive.google.com/file/d/17nXxjx6-ixUfvbLtWHky4BaJLeF9DzA4/view?usp=drivesdk",
+				"https://drive.google.com/file/d/1aQwG-eCYM6MMQ46pvtTANF99GFULvT_x/view?usp=drivesdk",
+				"https://drive.google.com/file/d/14H2NJIWbjRZWh49cOCkVz_gKL511Fx_u/view?usp=drivesdk",
 				"https://drive.google.com/file/d/1mwFsZFeridcH5MekuQObXN1EB5aIv69u/view?usp=drivesdk",
 				"https://drive.google.com/file/d/1GG4NGX7vJCRMZ3o40sDS6JuobwIY-p5q/view?usp=drivesdk",
 				"https://drive.google.com/file/d/1yy6NrgpziSMkFSV6_ru0dxvj_znWz8dq/view?usp=drivesdk",
 				"https://drive.google.com/file/d/15lv0-lIzT0N8F560dYXh3I7kDi8OUPOL/view?usp=drivesdk",
 				"https://drive.google.com/file/d/1N8ImFuV28N2brcHYTXly0i2zyeRZBmi-/view?usp=drivesdk",
 				"https://drive.google.com/file/d/1BUWa1rcb1_79fCf_AK22ZUO8z0eY599u/view?usp=drivesdk",
-				"https://drive.google.com/file/d/14Ww3kpsy_3cRBq76YNxvWqkphWimZDe-/view?usp=drivesdk",
-				"https://drive.google.com/file/d/1Agoxklr7EjdBlGxbAmyq29Vwroithj6Y/view?usp=drivesdk",
-				"https://drive.google.com/file/d/1T56P0C1YJ92Uez32bWw8sw0oOodZ7j_b/view?usp=drivesdk",
-				"https://drive.google.com/file/d/14j9hLtKfwnc-h-57j0m5yXYZxga2Zu3A/view?usp=drivesdk",
-				"https://drive.google.com/file/d/1bM84zxL3b5LLepr6LyFLRoYaviKUafXF/view?usp=drivesdk",
-				"https://drive.google.com/file/d/1WQD3dE-8CmB_bCVfwmipIIciBlv0lWeY/view?usp=drivesdk",
-				"https://drive.google.com/file/d/13DCUDHTYqb92Il4VKCujozFdOohRn-qY/view?usp=drivesdk",
-				"https://drive.google.com/file/d/1Kt-Un53274hKCvb7U_xeN_GOc6PGdQc6/view?usp=drivesdk",
-				"https://drive.google.com/file/d/1OafQumSW9S1OJBH7j6tsDoG9ChnNPBmj/view?usp=drivesdk",
-				"https://drive.google.com/file/d/1YzW-v6ABytt5MvdCtVVqYKxA9hfGwzr1/view?usp=drivesdk",
-				"https://drive.google.com/file/d/1oQ-xoLdejTJglVobvD927DpRH3VN55Pu/view?usp=drivesdk",
-				"https://drive.google.com/file/d/1sZiYb5_6LwMGImdtNSqUhuiqSSJuQf_w/view?usp=drivesdk",
-				"https://drive.google.com/file/d/1UaC0Q7usa8TGet9fIhJ2rTu5O4w04xLP/view?usp=drivesdk",
-				"https://drive.google.com/file/d/1y5qu9vwryToPorX9sxqDqAs5o2B1_K5X/view?usp=drivesdk",
-				"https://drive.google.com/file/d/1xuElrbrJLhMK0c14XkuOb_M84MY4vORc/view?usp=drivesdk",
-				"https://drive.google.com/file/d/1wlOASkxyCOak4TY1XsrDxI5-IRatyU9p/view?usp=drivesdk",
-				"https://drive.google.com/file/d/1uJlqIJPuBZs5ZS4Z80pMBZ4wNRX8UWMC/view?usp=drivesdk",
-				"https://drive.google.com/file/d/1KqzbZFCwU1t6ZhYE_Lc8dzIuj3txEnJf/view?usp=drivesdk",
-				"https://drive.google.com/file/d/1V5P3rZYf-JRVnQS2kBA6stSKjyG7uZiD/view?usp=drivesdk",
-				"https://drive.google.com/file/d/1Z5cWrMQD61PB-F_PpNDxDE-Iv2U5Jsiz/view?usp=drivesdk",
-				"https://drive.google.com/file/d/1_DgW0RP82p-UiCLqw8JLEniSKyHmuLOy/view?usp=drivesdk",
-				"https://drive.google.com/file/d/1XF3X1dofiDvQlMf3VyFFKWlKndgc0e9B/view?usp=drivesdk",
-				"https://drive.google.com/file/d/10M3eKhi-LZvRR6POhYiUZOsCkvOXMkhQ/view?usp=drivesdk",
-				"https://drive.google.com/file/d/1i8Qv5qbWxTLStmJ5sE3hcBIXzspfimvB/view?usp=drivesdk",
-				"https://drive.google.com/file/d/1xLQjcw_r7wHNNWsEadbsZt1AXlzszKet/view?usp=drivesdk",
-				"https://drive.google.com/file/d/12xfbhbV3L60UYBXyXkkxLcmvkTDHKELq/view?usp=drivesdk",
-				"https://drive.google.com/file/d/1vp6VCJ867iUHqpiBOC0-OA3F0W8qoSVz/view?usp=drivesdk",
-				"https://drive.google.com/file/d/15QLh5aAfEBoN4XJhziOpamFCqGmj8P6s/view?usp=drivesdk",
-				"https://drive.google.com/file/d/13aFTqe_AhPmyuTtAnFPh2yyzNfwD_smp/view?usp=drivesdk",
-				"https://drive.google.com/file/d/13APo4k7GzhobbTqftvOT0TM1cNxA1E1Y/view?usp=drivesdk",
-				"https://drive.google.com/file/d/1nvBTxXZ8vfgQ-ogLXJ46wM05alASCl9O/view?usp=drivesdk",
-				"https://drive.google.com/file/d/12pSlqN2jQHUoCUzQ2Yj1tTkrp9WfexLT/view?usp=drivesdk",
-				"https://drive.google.com/file/d/1MmL976M8VJJRTbuQRjbIC1nkKrej64PW/view?usp=drivesdk"
+				"https://drive.google.com/file/d/14Ww3kpsy_3cRBq76YNxvWqkphWimZDe-/view?usp=drivesdk"
 				
 				
 				
